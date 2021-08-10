2126a87a5c7eeb0c9306966192094998f6743c05
Michael Y <meyan985@outlook.com>
2021-08-10 10:03:30 -0700

    Created page and methods to add a rule via GraphQL mutation to Rule service

---
+++ src/EmailView/Pages/AddRule.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EmailView.DataServices;
+using EmailView.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace EmailView.Pages
+{
+    public partial class AddRule
+    {
+        [Inject]
+        IAlertService AlertService { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        IRuleDataService RuleService { get; set; }
+
+        [Inject]
+        StackService MailService { get; set; }
+
+        private string ruleName;
+        private RuleDto addedRule = new RuleDto();
+
+        // private ConditionDto addedCondition = new ConditionDto();
+
+        // private ActionDto addedAction = new ActionDto();
+
+        public bool LoggedIn
+        {
+            get { return MailService.IsAuthenticated; }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            // rules = await RuleService.GetAllRules();
+        }
+
+        private async void HandleValidSubmit()
+        {
+            Console.WriteLine("HandleValidSubmit called");
+
+            // Process the valid form
+            var result = await RuleService.AddRule(ruleName);
+            if(result != null)
+            {
+                addedRule = result;
+                // StateHasChanged();
+                // NavigationManager.NavigateTo("/roomzfront/1");
+            }
+        }
+    }
+}
